459c1638b1a48f02f8d6c444833be93d5d0e6e18
Ignacio Perez <ignacio.perez2123@protonmail.com>
2024-09-08 14:53:10 -0300

    Added config for librewolf

--- home-manager/global/browser.nix
+++ home-manager/global/browser.nix
@@ -8,7 +8,15 @@
         "privacy.resistFingerprinting" = false;
         "privacy.clearOnShutdown.history" = false;
         "privacy.clearOnShutdown.cookies" = false;
+        "privacy.clearHistory.cache" = false;
+        "privacy.clearHistory.cookiesAndStorage" = false;
+        "privacy.clearHistory.historyFormDataAndDownloads" = false;
+        "privacy.clearOnShutdown_v2.cache" = false;
+        "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
+        "privacy.history.custom" = true;
         "network.cookie.lifetimePolicy" = 0;
+
+        "identity.fxaccounts.enabled" = true;
 
         "sidebar.verticalTabs" = true;
         "sidebar.revamp" = true;
@@ -87,6 +95,15 @@
       "x-scheme-handler/https" = [ "librewolf.desktop" ];
       "x-scheme-handler/about" = [ "librewolf.desktop" ];
       "x-scheme-handler/unknown" = [ "librewolf.desktop" ];
+    };
+
+    associations.removed = {
+      "default-web-browser" = [ "brave.desktop" ];
+      "text/html" = [ "brave.desktop" ];
+      "x-scheme-handler/http" = [ "brave.desktop" ];
+      "x-scheme-handler/https" = [ "brave.desktop" ];
+      "x-scheme-handler/about" = [ "brave.desktop" ];
+      "x-scheme-handler/unknown" = [ "brave.desktop" ];
     };
   };
 }
