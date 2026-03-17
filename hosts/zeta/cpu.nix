524606a566ff686365a6ad8169ffbe53e7c4ccb7
Ignacio Perez <ignacio@feuer.me>
2026-03-17 20:45:45 -0300

    Updated zeta config

--- hosts/zeta/cpu.nix
+++ hosts/zeta/cpu.nix
@@ -6,11 +6,11 @@
     tlp = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
 
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 65;
@@ -20,8 +20,8 @@
         CPU_BOOST_ON_AC = 0;
         CPU_BOOST_ON_BAT = 0;
 
-        PLATFORM_PROFILE_ON_AC = "balanced";
-        PLATFORM_PROFILE_ON_BAT = "low-power";
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "balanced";
 
         WIFI_PWR_ON_AC = "on";
         WIFI_PWR_ON_BAT = "on";
