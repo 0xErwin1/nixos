a737af2850b20668a6a17b7a69ca1bd735a9701c
Ignacio Perez <ignacio@feuer.me>
2026-03-18 18:56:33 -0300

    Fix flameshot

--- home-manager/global/x11/default.nix
+++ home-manager/global/x11/default.nix
@@ -27,4 +27,38 @@
       enable = true;
     };
   };
+
+  # Flameshot service to run in background
+  # This ensures clipboard functionality works correctly in X11
+  systemd.user.services.flameshot = {
+    Unit = {
+      Description = "Flameshot screenshot tool";
+      After = [ "graphical-session-pre.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.flameshot}/bin/flameshot";
+      Restart = "on-failure";
+      RestartSec = 3;
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
+
+  # Flameshot configuration for clipboard support
+  xdg.configFile."flameshot/flameshot.ini".text = ''
+    [General]
+    disabledTrayIcon=false
+    showStartupLaunchMessage=false
+    copyPathAfterSave=false
+    saveAfterCopy=false
+    savePathFixed=false
+    useJpgForClipboard=false
+
+    [Shortcuts]
+    TYPE_COPY=Ctrl+C
+  '';
 }
